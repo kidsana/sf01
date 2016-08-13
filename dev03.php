49768f93c0ec0c16d2e95a400a18f39f4209da84
BeerWiriya <beerwiriya37@gmail.com>
2016-08-13 08:55:11 +0700

    TestGit 13/08/2559

--- dev03.php
+++ dev03.php
@@ -1,1 +1,16 @@
-<?php ?>
+<?php 
+
+if (isset($_FILES['uploaded_file'])) {
+    // Example:
+    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], "datas/" . $_FILES['uploaded_file']['name'])){
+        echo $_FILES['uploaded_file']['name']. " uploaded ...";
+    } else {
+        echo $_FILES['uploaded_file']['name']. " NOT uploaded ...";
+    }
+
+    exit;
+} else {
+    echo "no";
+
+
+?>
